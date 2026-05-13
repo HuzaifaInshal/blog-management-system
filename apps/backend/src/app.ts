abed12fda0863a54505aab872883b3b973332fc7
Huzaifa Inshal <huzaifainshal@gmail.com>
2026-05-14 03:10:58 +0500

    `Chore` - vercel configuration setup

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -7,7 +7,12 @@ import blogRoutes from "./modules/blog/blog.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL ?? "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
